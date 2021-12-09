738cebd8f75eea3bd37f21677e716deb6777f8c2
Konrad Private <konrad.wrobel@gmail.com>
2021-12-09 17:22:01 +0000

    Slices mid way

--- 04_array_and_slices/sum.go
+++ 04_array_and_slices/sum.go
@@ -7,3 +7,12 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+// I was close to the below function but not getting in entirely
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
--- 04_array_and_slices/sum_test.go
+++ 04_array_and_slices/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("slice of 5 numbers", func(t *testing.T) {
@@ -13,4 +16,15 @@ func TestSum(t *testing.T) {
 			t.Errorf("got : %d, want: %d, given: %v ", got, want, numbers)
 		}
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+	// want := "bob"
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %d, want: %d", got, want)
+	}
+
 }
